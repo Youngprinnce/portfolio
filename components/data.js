6a84b39400f9f9ec9257859a42e1ed5d534bf6d6
Ajiboye Adedotun <ajiboyeadedotun16@gmail.com>
2022-04-13 18:20:57 +0100

    init deploy

---
+++ components/data.js
@@ -0,0 +1,38 @@
+export const projectsList = [
+  {
+    id: 1,
+    name: "Metricks",
+    description:
+      "Metricks is an affiliate tracking software developed to provide small and medium scale enterprises with the solution they need to grow and scale their affiliate marketing.",
+    image: "/images/metricks.png",
+    mobileImage: "/images/metricks.png",
+    link: "https://www.metricks.io/",
+    GitHub: "",
+    project: "Metricks",
+    tools: ["Node.js", "Express.js", "PostgreSQL", "TypeScript", "Mocha", "Chai", "AWS"],
+  },
+  {
+    id: 2,
+    name: "Covid-19 Tracker",
+    description:
+      "Covid-19 Tracker is a web application that allows users to track the latest updates on the COVID-19 pandemic. Its main purpose is to provide a platform for users to stay informed about the latest updates on the virus. Its features a light and dark mode, a search bar, and a filter bar.",
+    image: "/images/covid-19.png",
+    mobileImage: "/images/covid-19.png",
+    link: "https://covid-19-tracker-3317f.web.app/",
+    GitHub: "https://github.com/Youngprinnce/covid-19-tracker",
+    project: "Covid-19 Tracker",
+    tools: ["React.js", "Material-Ui", "Chart.js", "Firebase"],
+  },
+  {
+    id: 3,
+    name: "File-Share",
+    description:
+      "File-Share is a web application that allows users to share files with other users. This is a mini app that allows you share files of up to100mb between your family and friends. All you do is upload the file and you get the link to the uploaded file, you can also send an the file as an email to anyone you like.",
+    image: "/images/file-share.png",
+    mobileImage: "/images/file-share.png",
+    link: "https://youngprinnce.github.io/file-share-frontend/",
+    GitHub: "https://github.com/Youngprinnce/file-share",
+    project: "FIle-Share",
+    tools: ["Node.js", "HTML", "CSS", "JavaScript"],
+  },
+];
